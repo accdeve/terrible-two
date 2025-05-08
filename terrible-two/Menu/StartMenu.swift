4497e6c311e2f1e7de2e59b8a58e396e1068ec58
accdeveadit <acdeveadit@gmail.com>
2025-05-09 01:13:36 +0700

    add start menu and navigation quit and pay game

--- terrible-two/Menu/StartMenu.swift
+++ terrible-two/Menu/StartMenu.swift
@@ -1,53 +1,170 @@
-//
 //  StartMenu.swift
 //  terrible-two
 //
 //  Created by aditya ibnu arif on 08/05/25.
 //
+
 import SwiftUI
 
 struct GameStartView: View {
     @State private var showStartText = true
+    @State private var navigateToGame = false
+    @State private var isShowingPopUp = false
     let blinkDuration: TimeInterval = 0.5
 
     var body: some View {
-        ZStack {
-            // Background image full screen
-            Image("level1_background")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
+        NavigationStack {
+            ZStack {
+                // NavigationLink yang tersembunyi
+                NavigationLink(
+                    destination: Level1View(), isActive: $navigateToGame
+                ) {
+                    EmptyView()
+                }
 
-            Color.black
-                .ignoresSafeArea()
-                .opacity(0.7)
+                // Background image full screen
+                Image("level1_background")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
 
-            Text("Terrible Two")
-                .font(.system(size: 36, weight: .bold, design: .rounded))
-                .foregroundColor(.white)
-                .position(
-                    x: UIScreen.main.bounds.midX,
-                    y: UIScreen.main.bounds.midY - 150)
+                Color.black
+                    .ignoresSafeArea()
+                    .opacity(0.7)
 
-            Text("Start Game")
-                .font(.system(size: 24, weight: .bold, design: .rounded))
-                .foregroundColor(.yellow)
-                .opacity(showStartText ? 1 : 0)
-                .position(
-                    x: UIScreen.main.bounds.midX,
-                    y: UIScreen.main.bounds.midY + 150
-                )
-                .animation(.easeInOut(duration: 0.5), value: showStartText)
+                Text("Terrible Two")
+                    .font(.system(size: 52, weight: .bold, design: .rounded))
+                    .foregroundColor(.white)
+                    .position(
+                        x: UIScreen.main.bounds.midX,
+                        y: UIScreen.main.bounds.midY - 150)
 
-        }
-        .onAppear {
-            Timer.scheduledTimer(withTimeInterval: blinkDuration, repeats: true)
-            { _ in
-                withAnimation {
-                    showStartText.toggle()
+                ZStack {
+                    Image("Button_White")
+                        .resizable()
+                        .frame(width: 250, height: 170)
+                        .position(
+                            x: UIScreen.main.bounds.midX,
+                            y: UIScreen.main.bounds.midY + 50
+                        )
+                    Text("Play Game")
+                        .font(
+                            .system(size: 24, weight: .bold, design: .rounded)
+                        )
+                        .foregroundColor(.black)
+                        .opacity(showStartText ? 1 : 0)
+                        .position(
+                            x: UIScreen.main.bounds.midX,
+                            y: UIScreen.main.bounds.midY + 70
+                        )
+
+                }.onTapGesture {
+                    navigateToGame = true
+                }
+
+                ZStack {
+                    Image("Button_White")
+                        .resizable()
+                        .frame(width: 250, height: 170)
+                        .position(
+                            x: UIScreen.main.bounds.midX,
+                            y: UIScreen.main.bounds.midY + 100
+                        )
+                    Text("Settings")
+                        .font(
+                            .system(size: 24, weight: .bold, design: .rounded)
+                        )
+                        .foregroundColor(.black)
+                        .opacity(showStartText ? 1 : 0)
+                        .position(
+                            x: UIScreen.main.bounds.midX,
+                            y: UIScreen.main.bounds.midY + 120
+                        )
+                        .onTapGesture {
+                            navigateToGame = true
+                        }
+
+                }.onTapGesture {
+                    navigateToGame = true
+                }
+
+                ZStack {
+                    Image("Button_White")
+                        .resizable()
+                        .frame(width: 250, height: 170)
+                        .position(
+                            x: UIScreen.main.bounds.midX,
+                            y: UIScreen.main.bounds.midY + 150
+                        )
+                    Text("Quit")
+                        .font(
+                            .system(size: 24, weight: .bold, design: .rounded)
+                        )
+                        .foregroundColor(.red)
+                        .opacity(showStartText ? 1 : 0)
+                        .position(
+                            x: UIScreen.main.bounds.midX,
+                            y: UIScreen.main.bounds.midY + 170
+                        )
+
+                }.onTapGesture {
+                    isShowingPopUp = true
+                }
+
+                if isShowingPopUp {
+                    VStack {
+                        Text("Quit Game")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding(.top, 20)
+
+                        Spacer()
+
+                        HStack {
+                            Text("Quit")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.red)
+                                .cornerRadius(10)
+                                .onTapGesture {
+                                    exit(0)
+                                }
+
+                            Spacer().frame(width: 10)
+
+                            Text("Cancel")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.green)
+                                .cornerRadius(10)
+                                .onTapGesture {
+                                    isShowingPopUp = false
+                                }
+
+                        }
+                        .padding(.bottom, 20)
+                        .padding(.horizontal, 20)
+                    }
+                    .frame(width: 300, height: 200)  // Adjust the size for a widget-like view
+                    .background()
+                    .cornerRadius(12)
+                    .position(
+                        x: UIScreen.main.bounds.midX,
+                        y: UIScreen.main.bounds.midY
+                    )
                 }
             }
         }
+    }
+}
+
+struct GameView: View {
+    var body: some View {
+        Text("Game View")
+            .font(.largeTitle)
+            .foregroundColor(.black)
     }
 }
 
